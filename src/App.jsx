36943bccaaa1887e0e848905e1b97326af4f33c1
Guillaume Mardinli <guillaume.mardinli@ynov.com>
2025-07-29 09:18:10 +0200

    fix little margin index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Timeline from './components/Timeline';
 import Presentation from './components/Presentation';
